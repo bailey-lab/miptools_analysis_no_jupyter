0a97519c1e64fa8ba5d6d915a1c4f4cf902aa9d4
Alfred Simkin <alfred.simkin@gmail.com>
2023-07-12 18:26:22 +0000

    set freebayes threads

--- variant_calling/scripts/call_variants_freebayes.py
+++ variant_calling/scripts/call_variants_freebayes.py
@@ -7,8 +7,11 @@ wdir=snakemake.params['wdir']
 settings_file=snakemake.params['settings_file']
 options=snakemake.params['freebayes_settings']
 processor_number=snakemake.params.processor_number
+freebayes_threads=snakemake.params.freebayes_threads
 settings = mip.get_analysis_settings(wdir+'/'+settings_file)
 
+
+settings['freebayes_threads']=freebayes_threads
 settings['processorNumber']=processor_number
 verbose=True
 fastq_dir="/opt/analysis/padded_fastqs"
@@ -19,7 +22,7 @@ errors_file="/opt/analysis/freebayes_errors.txt"
 warnings_file="/opt/analysis/freebayes_warnings.txt"
 
 #what is the purpose of the variable 'r'? I don't think it ever gets used again.
-subprocess.call('ulimit -n 10000', shell=True)
+subprocess.call('ulimit -Sn $(ulimit -Hn)', shell=True)
 r = mip.freebayes_call(settings=settings, options=options, align=True,
 verbose=True, fastq_dir=fastq_dir, bam_dir=bam_dir, vcf_file=vcf_file,
 targets_file=targets_file, bam_files=None, errors_file=errors_file,
