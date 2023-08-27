010120b1ef9c9293e7c1cce1b224516a01dfd55f
Alfred Simkin <alfred.simkin@gmail.com>
2023-08-27 23:00:45 +0000

    consolidated version

---
+++ scripts/call_variants_freebayes.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.append("/opt/src")
+import mip_functions as mip
+
+wdir=snakemake.params['wdir']
+settings_file=snakemake.params['settings_file']
+options=snakemake.params['freebayes_settings']
+settings = mip.get_analysis_settings(wdir+'/'+settings_file)
+
+verbose=True
+fastq_dir="/opt/analysis/padded_fastqs"
+bam_dir="/opt/analysis/padded_bams"
+vcf_file="/opt/analysis/variants.vcf.gz"
+targets_file="/opt/project_resources/targets.tsv"
+errors_file="/opt/analysis/freebayes_errors.txt"
+warnings_file="/opt/analysis/freebayes_warnings.txt"
+
+#what is the purpose of the variable 'r'? I don't think it ever gets used again.
+r = mip.freebayes_call(settings=settings, options=options, align=True,
+verbose=True, fastq_dir=fastq_dir, bam_dir=bam_dir, vcf_file=vcf_file,
+targets_file=targets_file, bam_files=None, errors_file=errors_file,
+warnings_file=warnings_file, fastq_padding=20)
+
+contig_dict_list, results, errors=r
+for contig_dict in contig_dict_list:
+	print(contig_dict)
